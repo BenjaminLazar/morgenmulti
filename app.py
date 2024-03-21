f5a14c4effec51882ef5c2f5f04532eb683ad5fb
Benjamin Lazar <113016359+BenjaminLazar@users.noreply.github.com>
2024-03-21 03:26:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,19 @@ import json
 st.image("todai 1.png")
 
 # Text input
-user_text = st.text_input("Skriv tanker eller overvejelser omkring talen her:")
+user_text = st.text_input("Add thoughts or notes for the questions here:")
 
 
 # Image input
-uploaded_file = st.file_uploader("Vælg billede", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Choose Image", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
 
 def encode_image(image):
+    img_format = image.format if image.format in ['JPEG', 'PNG'] else 'JPEG'  # Default to JPEG if format is not JPEG or PNG
     buffered = io.BytesIO()
-    image.save(buffered, format="JPEG")
+    image.save(buffered, format=img_format)
     img_str = base64.b64encode(buffered.getvalue()).decode()
     return img_str
 
@@ -29,7 +30,7 @@ if uploaded_file is not None:
 
 # Function to send data to ChatGPT Vision
 def send_to_chatgpt_vision(text, image_base64):
-    api_key = st.secrets["OPENAI_API_KEY"]
+    api_key = "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"
     extra_prompt = "You help participents at a talk to generate questions with the help of an image of the talk. Todays talk is about large multimodal AI models. Its a talk that makes a simple explenation on what multimodality is and what these models are, and then focusses on what uses cases that exists in everyday buisness tasks. Generate questions for this talk based on what you know, the image, and some extra notes that comes here: "
     headers = {
         "Content-Type": "application/json",
@@ -60,7 +61,7 @@ def send_to_chatgpt_vision(text, image_base64):
     return response.json()
 
 # Button to send request
-if st.button('Send to ChatGPT Vision'):
+if st.button('Generate Questions'):
     if user_text and uploaded_file is not None:
         response = send_to_chatgpt_vision(user_text, base64_image)
         st.write(response['choices'][0]['message']['content'])
